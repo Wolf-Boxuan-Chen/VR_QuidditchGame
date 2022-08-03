b26724790965919fea0e10dcc6f0b7f1d4d6b446
wolfboxuan <boxuan@wustl.edu>
2022-08-02 20:33:02 -0400

    Quest Link & Hand Basic Animation

---
+++ VR_QuidditchGame_Game/Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimation : MonoBehaviour
+{
+    [SerializeField] InputActionReference triggerInputAction;
+    [SerializeField] InputActionReference gripInputAction;
+
+    Animator handAnimator;
+
+    private void Awake() 
+    {
+        handAnimator = GetComponent<Animator>();
+    }
+
+    private void OnEnable() 
+    {
+        triggerInputAction.action.performed += TriggerPressed;
+        gripInputAction.action.performed += GripPressed;
+    }
+
+    private void GripPressed(InputAction.CallbackContext obj)
+    {
+        handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+    }
+
+    private void TriggerPressed(InputAction.CallbackContext obj)
+    {
+        handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+    }
+
+    private void OnDisable() 
+    {
+        triggerInputAction.action.performed -= TriggerPressed;
+        gripInputAction.action.performed -= GripPressed;
+    }
+}
